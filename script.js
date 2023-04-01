7a821672f464fced836e0f52e7186367e20f1a47
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-01 14:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let variable1 = prompt("1st variable"); 
+variable1 = variable1 * 2;
+let variable2 = prompt("2nd variable");
+let res = parseInt(variable1) + parseInt(variable2);
+alert("The final result is: " + res);
